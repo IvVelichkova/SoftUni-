729bd09a9f463d7fbfb77dc0edc18f2dada40843
IvVelichkova <night_princess.1986@abv.bg>
2017-06-20 14:22:03 +0300

    Problem 4.Count Symbols
    Problem 4.Count Symbols

---
+++ StartUpCountSymbols.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSymbols
+{
+    /*Write a program that reads some text from the console and counts the occurrences of each character in it. Print the results in alphabetical (lexicographical) order. */
+    class StartUpCountSymbols
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            var dictionary = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!dictionary.ContainsKey(input[i]))
+                {
+                    dictionary.Add(input[i], 1);
+                }
+                else
+                {
+                    dictionary[input[i]]++;
+                }
+            }
+            foreach (var ch in dictionary)
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}
