c44eb9555240a4fd4a5300b6cab757815fa9d3ab
IvVelichkova <night_princess.1986@abv.bg>
2017-06-20 14:11:32 +0300

    Add files via upload

---
+++ ExtractIntegerNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExtractIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Regex regex = new Regex("\\d+");
+            MatchCollection mathces = regex.Matches(text);
+
+            foreach (var match in mathces)
+            {
+                Console.WriteLine($"{match}");
+            }
+            
+        }
+    }
+}
